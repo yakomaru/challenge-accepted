05735276bf056504934efd65152c606debe7fd31
Alex Rodriguez <al3xelias@gmail.com>
2015-08-24 01:02:45 -0400

    Add AllChallenges Controller -Invoke function to fetch all challenges from db -Return all challenges after pushing to array

--- public/modules/challenges-page/controllers/all-challenges.client.controller.js
+++ public/modules/challenges-page/controllers/all-challenges.client.controller.js
@@ -1,8 +1,24 @@
 'use strict';
 
-angular.module('challenges-page').controller('AllChallengesController', ['$scope',
-	function($scope) {
-		// All challenges controller logic
-		// ...
-	}
+angular.module('challenges-page').controller('AllChallengesController', ['$scope', 'Authentication', 'Todo',
+	function($scope, Authentication, Todo, $location) {
+
+    $scope.authentication = Authentication;
+
+    var getAllChallenges = function(){
+      Todo.getAllChallenges()
+      .then(function(res){
+        $scope.allChallenges = [];
+        for (var i = 0; i < res.data.length; i++){
+            $scope.allChallenges.push(res.data[i]);
+          }
+      }, function(err){
+        console.log(err);
+      });
+
+      return $scope.allChallenges;
+    };
+
+    getAllChallenges();
+  }
 ]);
